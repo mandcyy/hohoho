0f140a1deb9bb0e3dea3d5535b43c25beb354312
mandcyy <mandcyy@gmail.com>
2026-04-29 16:59:41 +0800

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: '', timeout: 10000 });
+const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || '', timeout: 10000 });
 api.interceptors.request.use(cfg => {
   const t = localStorage.getItem('token');
   if (t) cfg.headers.Authorization = `Bearer ${t}`;
